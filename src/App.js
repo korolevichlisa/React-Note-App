7dc6c925e74d9fa29916fa158c6c4b791da06a2a
korolevichlisa <korolevich.liza@gmail.com>
2023-08-07 17:44:03 +0300

    added statictical tabel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [inputData, setInputData] = useState("");
+  const [inputTextDes, setInputTextDes] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [category, setCategory] = useState("Task");
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [note, setNote] = useState(null);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "Archive":
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case "Active":
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+    filterHandler();
+  }, [todos, status]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        inputTextDes={inputTextDes}
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setInputTextDes={setInputTextDes}
+        setStatus={setStatus}
+        setCategory={setCategory}
+        category={category}
+        setNote={setNote}
+        note={note}
+        setInputData={setInputData}
+        inputData={inputData}
+      />
+
+      <TodoList
+        status={status}
+        setNote={setNote}
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+        setInputText={setInputText}
+        setInputTextDes={setInputTextDes}
+        setInputData={setInputData}
+        inputData={inputData}
+      />
     </div>
   );
 }
